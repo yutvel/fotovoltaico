02ed5e9a7c46aaaaa71505d0665891cd55e04eb6
Yutvel Lopez Alvarez <36247093+yutvel@users.noreply.github.com>
2022-01-24 20:35:08 +0000

    Initial commit

---
+++ src/app/modules/main/menu-sidebar/menu-sidebar.component.ts
@@ -0,0 +1,43 @@
+import {Component, OnInit} from '@angular/core';
+import {AppService} from '@services/app.service';
+
+@Component({
+    selector: 'app-menu-sidebar',
+    templateUrl: './menu-sidebar.component.html',
+    styleUrls: ['./menu-sidebar.component.scss']
+})
+export class MenuSidebarComponent implements OnInit {
+    public user;
+    public menu = MENU;
+
+    constructor(public appService: AppService) {}
+
+    ngOnInit() {
+        this.user = this.appService.user;
+    }
+}
+
+export const MENU = [
+    {
+        name: 'Dashboard',
+        path: ['/']
+    },
+    {
+        name: 'Blank',
+        path: ['/blank']
+    },
+    {
+        name: 'Main Menu',
+        children: [
+            {
+                name: 'Sub Menu',
+                path: ['/sub-menu-1']
+            },
+
+            {
+                name: 'Blank',
+                path: ['/sub-menu-2']
+            }
+        ]
+    }
+];
